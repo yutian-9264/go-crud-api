80b005313bcbb3aa331300360bc291d846935505
yutian <wqyutian@163.com>
2025-06-04 01:22:21 +0800

    create a new mux and register home route

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+
+	mux.Handle("/", &homeHandler{})
+
+	http.ListenAndServe(":8080", mux)
+}
+
+type homeHandler struct {
+}
+
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is the home page"))
 }
