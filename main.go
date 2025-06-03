a72878b20a3410c494319f1747417c8f63102c53
yutian <wqyutian@163.com>
2025-06-04 01:40:55 +0800

    add recipe route and define CRUD functions

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", &homeHandler{})
+	mux.Handle("/recipes", &recipesHandler{})
+	mux.Handle("/recipes/", &recipesHandler{})
 
 	http.ListenAndServe(":8080", mux)
 }
@@ -15,6 +17,23 @@ func main() {
 type homeHandler struct {
 }
 
+type recipesHandler struct {
+}
+
 func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("This is the home page"))
 }
+
+func (c *recipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is the recipe page"))
+}
+
+func (c *recipesHandler) CreateRecipe(w http.ResponseWriter, r *http.Request) {}
+
+func (c *recipesHandler) ListRecipes(w http.ResponseWriter, r *http.Request) {}
+
+func (c *recipesHandler) GetRecipe(w http.ResponseWriter, r *http.Request) {}
+
+func (c *recipesHandler) UpdateRecipe(w http.ResponseWriter, r *http.Request) {}
+
+func (c *recipesHandler) DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
